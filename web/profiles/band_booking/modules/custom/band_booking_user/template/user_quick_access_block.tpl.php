bd5b0afc67f08f2d3c55b82c795ff99b886f85c8
Romain Trenet <romain.trenet@gmail.com>
2018-06-29 14:34:26 +0200

    Clean features

--- web/profiles/band_booking/modules/custom/band_booking_user/template/user_quick_access_block.tpl.php
+++ web/profiles/band_booking/modules/custom/band_booking_user/template/user_quick_access_block.tpl.php
@@ -47,20 +47,14 @@
 <div id="<?php //print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
-<?php /*if ($block['subject']): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-<?php endif;*/ ?>
   <?php print render($title_suffix); ?>
 
   <div class="content"<?php print $content_attributes; ?>>
-    <?php //print $content ?>
     <?php
-
-    foreach($links as $link) {
-      //$link['options']['attributes']['class'] = 'yellow-btn btn';
-      $link['options']['attributes']['title'] = $link['text'];
-      print('<li>' . l($link['text'], $link['path'], $link['options']) . '</li>');
-    }
+        foreach($links as $link) {
+          $link['options']['attributes']['title'] = $link['text'];
+          print('<li>' . l($link['text'], $link['path'], $link['options']) . '</li>');
+        }
     ?>
   </div>
 </div>
