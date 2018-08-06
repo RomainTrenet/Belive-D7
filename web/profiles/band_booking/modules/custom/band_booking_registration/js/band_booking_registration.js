d598d1ff9099b6b44d1b128c50f5e2b8b061e11f
Romain Trenet <romain.trenet@gmail.com>
2018-08-06 18:02:57 +0200

    Select all behavior; No options;

--- web/profiles/band_booking/modules/custom/band_booking_registration/js/band_booking_registration.js
+++ web/profiles/band_booking/modules/custom/band_booking_registration/js/band_booking_registration.js
@@ -28,6 +28,7 @@
     }
   };
 
+  /*
   Drupal.ajax.prototype.commands.afterAjaxCallbackExample = function(ajax, response, status) {
     //auto open users dropwdown
     // @todo : use with multiple select library.
@@ -38,6 +39,33 @@
     //Show musicians, only if they are not already hide by the search input
     for (var musician_id in response.musicians_with_position){
       if (response.musicians_with_position.hasOwnProperty(musician_id)) {
+        var $li_parent = $select.find('input[value="' + musician_id + '"]').parents('li');
+        if (!$li_parent.hasClass("multiselect-filter-hidden")) {
+          $select.find('input[value="' + musician_id + '"]').parents('li').addClass('show-'+musician_id).show();
+        }
+      }
+    }
+
+    //Hide unwated musicians
+    for (var musician_id in response.musicians_to_hide){
+      if (response.musicians_to_hide.hasOwnProperty(musician_id)) {
+        $select.find('input[value="' + musician_id + '"]').parents('li').hide();
+      }
+    }
+  };
+  */
+  Drupal.ajax.prototype.commands.afterAjaxCallbackExample = function(ajax, response, status) {
+    // Temporary desactivated, Auto open users dropwdown.
+    // @todo.
+    // $('#register_many_users button.multiselect').trigger( "click" );
+
+    var $select = $("#register_many_users");
+    console.log(response);
+
+    // Show musicians, only if they are not already hide by the search input
+    for (var musician_id in response.musicians_with_position){
+      if (response.musicians_with_position.hasOwnProperty(musician_id)) {
+
         var $li_parent = $select.find('input[value="' + musician_id + '"]').parents('li');
         if (!$li_parent.hasClass("multiselect-filter-hidden")) {
           $select.find('input[value="' + musician_id + '"]').parents('li').addClass('show-'+musician_id).show();
--- web/profiles/band_booking/modules/custom/multiple_select/js/multiple-select.js
+++ web/profiles/band_booking/modules/custom/multiple_select/js/multiple-select.js
@@ -1,15 +1,14 @@
 (function($) {
 	Drupal.behaviors.multiple_select = {
 		attach: function (context, settings) {
-      //$('.multiple-select').multipleSelect();
       $('select.multiple-select').multipleSelect({
         width: '100%',
         filter: true,
-        //multiple: true,
-				placeholder: 'TRUC'
-        /*onOpen: function() {
-          //$eventResult.text('Select opened!');
-        },*/
+        multiple: false,
+				placeholder: Drupal.t('Select'),
+        selectAll: true,
+        selectAllText: Drupal.t('Select all'),
+        allSelected: Drupal.t('All selected'),
 			});
 		}
 	};
