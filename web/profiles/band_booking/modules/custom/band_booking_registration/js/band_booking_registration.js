ced289a47191d42ea6074f451d04ab76fd156916
Romain <romain.trenet@gmail.com>
2022-04-29 15:34:25 +0200

    DDEV docker + Remove former files

--- web/profiles/band_booking/modules/custom/band_booking_registration/js/band_booking_registration.js
+++ web/profiles/band_booking/modules/custom/band_booking_registration/js/band_booking_registration.js
@@ -3,57 +3,8 @@
   Drupal.behaviors.band_booking_registration = {
     attach: function (context, settings) {
 
-      //Only on add form
-      /*
-      @todo remove old code.
-      $('#edit-position').multiselect({
-        includeSelectAllOption: true,
-        selectAllText: ' Sélectionner tout',
-        nonSelectedText: 'Aucun sélectionné',
-        allSelectedText: 'Tous sélectionnés',
-        enableFiltering: true,
-        filterPlaceholder: 'Chercher',
-      });
-
-      //Global for add and remove form
-      $('#edit-add-users, #edit-remove-users').multiselect({
-        includeSelectAllOption: true,
-        selectAllText: ' Sélectionner tout',
-        nonSelectedText: 'Aucun sélectionné',
-        allSelectedText: 'Tous sélectionnés',
-        enableFiltering: true,
-        filterPlaceholder: 'Chercher'
-      });
-      */
     }
   };
-
-  /*
-  // @todo : remove old code.
-  Drupal.ajax.prototype.commands.afterAjaxCallbackExample = function(ajax, response, status) {
-    //auto open users dropwdown
-    $('#register_many_users button.multiselect').trigger( "click" );
-
-    var $select = $("#register_many_users");
-
-    //Show musicians, only if they are not already hide by the search input
-    for (var musician_id in response.musicians_with_position){
-      if (response.musicians_with_position.hasOwnProperty(musician_id)) {
-        var $li_parent = $select.find('input[value="' + musician_id + '"]').parents('li');
-        if (!$li_parent.hasClass("multiselect-filter-hidden")) {
-          $select.find('input[value="' + musician_id + '"]').parents('li').addClass('show-'+musician_id).show();
-        }
-      }
-    }
-
-    //Hide unwated musicians
-    for (var musician_id in response.musicians_to_hide){
-      if (response.musicians_to_hide.hasOwnProperty(musician_id)) {
-        $select.find('input[value="' + musician_id + '"]').parents('li').hide();
-      }
-    }
-  };
-  */
 
   Drupal.ajax.prototype.commands.afterAjaxCallbackExample = function(ajax, response, status) {
     var $select = $("#register_many_users");
